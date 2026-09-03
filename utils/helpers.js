bf93e1c96efc1a26486d904ba4d5f7fb168aa359
darkshinra154-lgtm <darkshinra154@gmail.com>
2026-09-03 19:12:37 +0300

    Add files via upload

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,39 +1,32 @@
-const { jidNormalizedUser } = require('@whiskeysockets/baileys');
+import { jidNormalizedUser } from '@whiskeysockets/baileys';
 
-function getConnectedBotNumbers(sessions) {
-    const numbers = [];
-    for (const session of Object.values(sessions)) {
-        if (session.sock?.user) {
-            numbers.push(jidNormalizedUser(session.sock.user.id).split('@')[0]);
-        }
-    }
-    return numbers;
+export function getConnectedBotNumbers(sessions) {
+const numbers = [];
+for (const session of Object.values(sessions)) {
+if (session.sock?.user) {
+numbers.push(jidNormalizedUser(session.sock.user.id).split('@')[0]);
+}
+}
+return numbers;
 }
 
-function getAllActiveSockets(sessions) {
-    const socks = [];
-    for (const [sessionId, session] of Object.entries(sessions)) {
-        if (session.sock && session.isConnected) {
-            socks.push({ sock: session.sock, sessionId, phoneNumber: session.phoneNumber });
-        }
-    }
-    return socks;
+export function getAllActiveSockets(sessions) {
+const socks = [];
+for (const [sessionId, session] of Object.entries(sessions)) {
+if (session.sock && session.isConnected) {
+socks.push({ sock: session.sock, sessionId, phoneNumber: session.phoneNumber });
+}
+}
+return socks;
 }
 
-function isPremiumUser(chatId, settings) {
-    const ownerChatId = process.env.OWNER_TELEGRAM_ID || settings.tgOwnerId;
-    return chatId.toString() === ownerChatId ||
-        (settings.premiumUsers && settings.premiumUsers.includes(chatId.toString()));
+export function isPremiumUser(chatId, settings) {
+const ownerChatId = process.env.OWNER_TELEGRAM_ID || settings.tgOwnerId;
+return chatId.toString() === ownerChatId ||
+(settings.premiumUsers && settings.premiumUsers.includes(chatId.toString()));
 }
 
-function isTgOwner(chatId, settings) {
-    const ownerChatId = process.env.OWNER_TELEGRAM_ID || settings.tgOwnerId;
-    return chatId.toString() === ownerChatId;
+export function isTgOwner(chatId, settings) {
+const ownerChatId = process.env.OWNER_TELEGRAM_ID || settings.tgOwnerId;
+return chatId.toString() === ownerChatId;
 }
-
-module.exports = {
-    getConnectedBotNumbers,
-    getAllActiveSockets,
-    isPremiumUser,
-    isTgOwner
-};
